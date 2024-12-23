e5c8ba096b88bda031d5bf25bd1a3687764b9192
toto <50374594+toine08@users.noreply.github.com>
2024-12-23 12:19:13 +0000

    Ajout du module Go et création d'un serveur HTTP de base

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/toine08/http-server
+
+go 1.23.4
---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import(
+	"net/http"
+)
+
+func main(){
+	mux := http.NewServeMux()
+	server := &http.Server{
+		Addr:":8080",
+		Handler:mux,
+	}
+	mux.HandleFunc("/", func(w http.ResponseWriter, req *http.Request){
+			http.NotFound(w,req)
+			return
+	})
+	server.ListenAndServe()
+}
