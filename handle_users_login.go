e0ae1b049ab131c6c62b29f8b1e2645616f95297
toine08 <riedotoine@gmail.com>
2025-01-03 14:25:05 +0100

    Add refresh token generation and update login response structure

--- handle_users_login.go
+++ handle_users_login.go
@@ -1,13 +1,14 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"net/http"
-	"os"
 	"time"
 
 	"github.com/google/uuid"
 	"github.com/toine08/http-server/internal/auth"
+	"github.com/toine08/http-server/internal/database"
 )
 
 func (cfg *apiConfig) handleLogin(w http.ResponseWriter, req *http.Request) {
@@ -15,14 +16,15 @@ func (cfg *apiConfig) handleLogin(w http.ResponseWriter, req *http.Request) {
 		Email            string `json:"email"`
 		Password         string `json:"password"`
 		ExpiresInSeconds *int   `json:"expires_in_seconds"` // Using a pointer to handle optionality
+
 	}
 	type response struct {
-		ID               uuid.UUID `json:"id"`
-		CreatedAt        time.Time `json:"created_at"`
-		UpdatedAt        time.Time `json:"updated_at"`
-		Email            string    `json:"email"`
-		ExpiresInSeconds int       `json:"expires_in_seconds"`
-		Token            string    `json:"token"`
+		ID            uuid.UUID `json:"id"`
+		CreatedAt     time.Time `json:"created_at"`
+		UpdatedAt     time.Time `json:"updated_at"`
+		Email         string    `json:"email"`
+		Token         string    `json:"token"`
+		Refresh_Token string    `json:"refresh_token"`
 	}
 
 	decoder := json.NewDecoder(req.Body)
@@ -39,26 +41,36 @@ func (cfg *apiConfig) handleLogin(w http.ResponseWriter, req *http.Request) {
 		return
 	}
 
-	expiresIn := time.Hour // Default to 1 hour
+	expireIn := time.Hour // Default to 1 hour
 	if params.ExpiresInSeconds != nil {
-		expiresIn = time.Duration(*params.ExpiresInSeconds) * time.Second
-		if expiresIn > time.Hour {
-			expiresIn = time.Hour
+		expireIn = time.Duration(*params.ExpiresInSeconds) * time.Second
+		if expireIn > time.Hour {
+			expireIn = time.Hour
 		}
 	}
 
-	token, err := auth.MakeJWT(user.ID, os.Getenv("TOKEN_SECRET"), expiresIn)
+	token, err := auth.MakeJWT(user.ID, cfg.tokenSecret, expireIn)
 	if err != nil {
 		respondWithError(w, http.StatusInternalServerError, "Error while creating the token", err)
 		return
 	}
+	refreshToken, err := auth.MakeRefreshToken()
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Sorry an error occured while generating the refresh token", err)
+	}
+	expireAt := time.Now().Add(60 * 24 * time.Hour) // 60 days from now
+	cfg.dbQueries.CreateRefreshToken(req.Context(), database.CreateRefreshTokenParams{
+		Token:     refreshToken,
+		UserID:    user.ID,
+		ExpiresAt: sql.NullTime{Time: expireAt, Valid: true},
+	})
 
 	respondWithJSON(w, http.StatusOK, response{
-		ID:               user.ID,
-		CreatedAt:        user.CreatedAt,
-		UpdatedAt:        user.UpdatedAt,
-		Email:            user.Email,
-		ExpiresInSeconds: int(expiresIn.Seconds()), // Convert duration to seconds
-		Token:            token,
+		ID:            user.ID,
+		CreatedAt:     user.CreatedAt,
+		UpdatedAt:     user.UpdatedAt,
+		Email:         user.Email,
+		Token:         token,
+		Refresh_Token: refreshToken,
 	})
 }
