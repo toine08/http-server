7d66ce55910b244084e8ea91cd67327dac198ed1
toine08 <riedotoine@gmail.com>
2025-01-04 18:54:28 +0100

    Implement sorting and filtering for chirps by author ID in API handler

--- handle_chirps_allchirps.go
+++ handle_chirps_allchirps.go
@@ -1,13 +1,47 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
 
-func (cfg *apiConfig) handleAllChirps(w http.ResponseWriter, r *http.Request) {
+	"github.com/google/uuid"
+	"github.com/toine08/http-server/internal/database"
+)
+
+func (cfg *apiConfig) handleAllChirps(w http.ResponseWriter, req *http.Request) {
 	var chirps []Chirp
-	rows, err := cfg.dbQueries.AllChirps(r.Context())
-	if err != nil {
-		respondWithError(w, 500, "Error retrieving data", err)
-		return
+	var rows []database.Chirp
+	var err error
+
+	s := req.URL.Query().Get("author_id")
+	order := req.URL.Query().Get("sort")
+
+	if s == "" {
+		if order == "desc" {
+			rows, err = cfg.dbQueries.AllChirpsDesc(req.Context())
+			if err != nil {
+				respondWithError(w, 500, "Error retrieving data", err)
+				return
+			}
+		} else {
+			rows, err = cfg.dbQueries.AllChirps(req.Context())
+			if err != nil {
+				respondWithError(w, 500, "Error retrieving data", err)
+				return
+			}
+		}
+	} else {
+		userID, err := uuid.Parse(s)
+		if err != nil {
+			respondWithError(w, 400, "Invalid author_id", err)
+			return
+		}
+		if order == "desc" {
+			rows, err = cfg.dbQueries.AllChirpsByUserIDDesc(req.Context(), userID)
+			if err != nil {
+				respondWithError(w, 500, "Error retrieving data", err)
+				return
+			}
+		}
 	}
 	for _, row := range rows {
 		var chirp Chirp
